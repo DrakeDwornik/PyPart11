77e0c5b4a0b17ee7be8fd781f5ca6d76a265c243
Drake Dwornik <drake@drakedwornik.com>
2021-12-13 23:00:31 -0500

    broken, turning in, just lost my Cohort class to a crash, remember it with fresh eyes

--- grade_book.py
+++ grade_book.py
@@ -9,6 +9,24 @@ class AliveStatus(Enum):
     Deceased = 0
     Alive = 1
 
+class CohortType(Enum):
+    Java = 0
+    Data = 1
+
+class Cohort():
+    def __init__(self, cohort_type: CohortType, major_num: int, minor_num: int):
+        if type(cohort_type) != CohortType:
+            logging.error('Cohort type must be a CohortType')
+            raise TypeError
+        else:
+            self.cohort_type = cohort_type
+        # try:
+        self.major_num = int(major_num)
+        self.minor_num = minor_num
+
+    def __repr__(self) -> str:
+        return f"{self.cohort_type.name} {self.major_num}.{self.minor_num}"
+
 
 class Person:
     def __init__(self, first_name: str, last_name: str, dob: tuple, alive: AliveStatus):
@@ -49,7 +67,27 @@ class Person:
         else:
             self.alive = status
 
+
 class Instructor(Person):
     def __init__(self, first_name: str, last_name: str, dob: tuple, alive: AliveStatus):
         super().__init__(first_name, last_name, dob, alive)
         self.instructor_id = f"Instructor_{uuid4()}"
+
+
+class Student(Person):
+    def __init__(self, first_name: str, last_name: str, dob: tuple, alive: AliveStatus):
+        super().__init__(first_name, last_name, dob, alive)
+        self.student_id = f"Student_{uuid4()}"
+
+
+class ZipCodeStudent(Student):
+    def __init__(self, first_name: str, last_name: str, dob: tuple, alive: AliveStatus, cohort: str):
+        super().__init__(first_name, last_name, dob, alive)
+        self.cohort = ""
+
+    def update_cohort(self, cohort):
+        if type(cohort) != str:
+            logging.error('Cohort must be a string')
+            raise TypeError
+        else:
+            self.cohort = cohort
--- test_grade_book.py
+++ test_grade_book.py
@@ -68,3 +68,88 @@ class TestInstructor(unittest.TestCase):
         testing_instructor.update_status(grade_book.AliveStatus.Deceased)
         self.assertEqual(grade_book.AliveStatus.Deceased, testing_instructor.alive)
         print(testing_instructor.instructor_id)
+
+
+class TestStudent(unittest.TestCase):
+    def test_update_first_name(self):
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        with self.assertRaises(TypeError):
+            testing_student.update_first_name(4)
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        testing_student.update_first_name("Jane")
+        self.assertEqual("Jane", testing_student.first_name)
+
+    def test_update_last_name(self):
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        with self.assertRaises(TypeError):
+            testing_student.update_last_name(4)
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        testing_student.update_last_name("Doe")
+        self.assertEqual("Doe", testing_student.last_name)
+
+    def test_update_dob(self):
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        with self.assertRaises(TypeError):
+            testing_student.update_dob(4)
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        testing_student.update_dob((2000, 1, 31))
+        self.assertEqual(date(2000, 1, 31), testing_student.dob)
+
+    def test_update_status(self):
+        testing_student = grade_book.Student("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive)
+        with self.assertRaises(TypeError):
+            testing_student.update_status(4)
+        testing_student.update_status(grade_book.AliveStatus.Deceased)
+        self.assertEqual(grade_book.AliveStatus.Deceased, testing_student.alive)
+        print(testing_student.student_id)
+
+
+class TestZipCodeStudent(unittest.TestCase):
+    def test_update_first_name(self):
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        with self.assertRaises(TypeError):
+            testing_zcw_student.update_first_name(4)
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        testing_zcw_student.update_first_name("Jane")
+        self.assertEqual("Jane", testing_zcw_student.first_name)
+
+    def test_update_last_name(self):
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        with self.assertRaises(TypeError):
+            testing_zcw_student.update_last_name(4)
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        testing_zcw_student.update_last_name("Doe")
+        self.assertEqual("Doe", testing_zcw_student.last_name)
+
+    def test_update_dob(self):
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        with self.assertRaises(TypeError):
+            testing_zcw_student.update_dob(4)
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        testing_zcw_student.update_dob((2000, 1, 31))
+        self.assertEqual(date(2000, 1, 31), testing_zcw_student.dob)
+
+    def test_update_status(self):
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        with self.assertRaises(TypeError):
+            testing_zcw_student.update_status(4)
+        testing_zcw_student.update_status(grade_book.AliveStatus.Deceased)
+        self.assertEqual(grade_book.AliveStatus.Deceased, testing_zcw_student.alive)
+        print(testing_zcw_student.student_id)
+
+    def test_update_cohort(self):
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        with self.assertRaises(TypeError):
+            testing_zcw_student.update_last_name(4)
+        testing_zcw_student = grade_book.ZipCodeStudent("John", "Smith", (1900, 12, 15), grade_book.AliveStatus.Alive,
+                                                        "Data 2.2")
+        testing_zcw_student.update_last_name("Java 1.0")
+        self.assertEqual("Java 1.0", testing_zcw_student.cohort)
